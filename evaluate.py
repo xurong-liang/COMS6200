b74aa9f51447254c20a61cf7f0b321e6490c2038
Jason Xurong Liang <xurong.liang@uqconnect.edu.au>
2022-08-21 14:59:35 +1000

    Change evaluate structure

---
+++ evaluate.py
@@ -0,0 +1,49 @@
+import os
+import sklearn.metrics
+
+
+def evaluate(classifier: str, hyper: str, y_true, y_preds,
+             start: int, end: int, data_method: str, res_dir: str = "./res/"):
+    """
+    Evaluate the performance of a classifier, output the evaluation result to
+    stdout and res_dir.
+
+    The output file will have the name of {classifier}_{hyper}.txt
+
+    :param classifier: Name of classifier
+    :param hyper: Name of parameter
+    :param y_preds: model predictions
+    :param y_true: ground truth labels
+    :param start: start of training time
+    :param end: end of training time
+    :param data_method: [unnormalized, minmax, zscore]
+    :param res_dir: the directory where the result is saved
+    """
+    if not os.path.exists(res_dir):
+        print(f"{res_dir} does not exists")
+        return
+
+    acc = sklearn.metrics.accuracy_score(y_true, y_preds)
+    precision = sklearn.metrics.precision_score(y_true, y_preds)
+    recall = sklearn.metrics.recall_score(y_true, y_preds)
+    f1 = sklearn.metrics.f1_score(y_true, y_preds)
+    train_time = f"{end - start:.6f}s"
+
+    text = ""
+    for name in ['classifier', 'hyper', 'precision', 'recall', 'f1', "train_time"]:
+        metric = locals()[name]
+        if type(metric) == str:
+            text += f'{name.replace("_", " ").title()}: {metric}\n'
+        else:
+            text += f'{name.replace("_", " ").title()}: {metric:.6f}\n'
+
+    print(text)
+
+    file_path = os.path.join(res_dir, f"{classifier}_{hyper}.txt")
+    with open(file_path, 'w') as f:
+        print(text, file=f)
+
+
+if __name__ == "__main__":
+    evaluate(classifier='svm', hyper="linear, lr_10", y_true=[1, 0, 0, 1], y_preds=[1, 0, 0, 0],
+             start=60, end=90, data_method='zscore')
