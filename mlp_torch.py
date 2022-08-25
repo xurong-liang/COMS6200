5d1736c625f32cabc2bfcf1d0de05de81abe2b61
NhatHuyNguyen <nhathuy.nguyen@gmail.com>
2022-08-25 11:07:30 +1000

    modify k fold train and test

--- mlp_torch.py
+++ mlp_torch.py
@@ -12,6 +12,16 @@ from tqdm import tqdm
 from evaluate import *
 import time
 
+def reset_weights(m):
+    """
+    Try resetting model weights to avoid
+    weight leakage.
+    """
+    for layer in m.children():
+        if hasattr(layer, 'reset_parameters'):
+            # print(f'Reset trainable parameters of layer = {layer}')
+            layer.reset_parameters()
+
 class MLP(nn.Module):
     def __init__(self, input_dim=58, output_dim=1):
         super().__init__()
@@ -55,21 +65,19 @@ class MyDataset(Dataset):
 
 
 class Runner():
-    def __init__(self, model, optimizer, loss_fn, epoch=1, batch_size=128, ordinal_label=0):
+    def __init__(self, epoch=2, batch_size=128, ordinal_label=0):
         self.ordinal_label = ordinal_label
         self.ds = MyDataset(ordinal_label=0)
-        self.model = model
-        self.optimizer = optimizer
-        self.loss_fn = loss_fn
         self.epoch = epoch
         self.batch_size = batch_size
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.model.to(self.device)
     
     def run(self):
-        folds = get_train_test_indices_for_all_folds(self.ds.get_df())
+        folds = get_train_test_indices_for_all_folds(self.ds.get_df(), k=2)
         start = time.time()
+        result_folds = []
         for fold, (train_idx, test_idx) in enumerate(folds):
+            
             print(f'---fold no----{fold+1}---')
             train_subsampler = torch.utils.data.SubsetRandomSampler(train_idx)
             test_subsampler = torch.utils.data.SubsetRandomSampler(test_idx)
@@ -78,68 +86,102 @@ class Runner():
                                 self.ds, 
                                 batch_size=self.batch_size,
                                 sampler=train_subsampler)
+
+            # faster to test on a single batch of every test data points
             test_loader = DataLoader(
                                 self.ds,
                                 batch_size=len(test_idx), 
                                 sampler=test_subsampler
                                 )
-
-            # model.apply(reset_weights)
+            model = MLP()
+            model.to(self.device)
+            model.apply(reset_weights)
+            optimizer = torch.optim.Adam(params=model.parameters())
+            loss_fn=F.binary_cross_entropy
 
             for epoch in range(self.epoch):
                 print(f'---epoch no---{epoch+1}---')
-                self.train(train_loader)
-                self.test(epoch, test_loader, test_idx)
+                self.train(train_loader, model, optimizer, loss_fn)
+                metrics_dict = self.test(epoch, test_loader, model, loss_fn, test_idx)
+                result_folds.append(metrics_dict)
 
         end = time.time()
-        
-        target, pred = self.test(fold, test_loader, test_idx, val=False)
-        if self.device.type == 'cuda':
-            target, pred = target.cpu(), pred.cpu()
-        class_metric = {}
-        dict_metric = compute_metric_values(target.detach().numpy(), pred.detach().numpy())
-        dict_metric["Training time"] = round(end - start, 2)
+            #     {
+            #     "accuracy": acc,
+            #     "precision": precision,
+            #     'recall': recall,
+            #     "f1": f1
+            #      }
+
+            # {
+            #     "normal": {
+            #                 "accuracy": acc,
+            #                 "precision": precision,
+            #                 'recall': recall,
+            #                 "f1": f1,
+            #                 "training time": training time
+            #             }
+            # }
+
+        dict_metrics = {}
+        for metric in ["accuracy", "precision", "recall", "f1"]:
+            dict_metrics[metric] = []
+
+        for result in result_folds:
+            for metric in ["accuracy", "precision", "recall", "f1"]:
+                dict_metrics[metric].append(result[metric])
+
+        for metric in ["accuracy", "precision", "recall", "f1"]:
+            all_metrics = dict_metrics[metric]
+            dict_metrics[metric] = np.asarray(all_metrics).mean()
+
+        dict_metrics["Training time"] = round(end - start, 2)
+
         class_name = self.ds.get_text_label()
-        class_metric[class_name] = dict_metric
-        perf_text = generate_class_performance_text(class_metric)
+        class_metrics = {}
+        class_metrics[class_name] = dict_metrics
+        perf_text = generate_class_performance_text(class_metrics)
         save_result_text(classifier="MLP", 
                         hyper="hidden_layer_80_100", 
                         data_method="minmax",
                         class_performance_text=perf_text)
 
 
-    def train(self, train_loader):
-        self.model.train()
-        for batch_idx, (data, target) in enumerate(tqdm(train_loader)):
+    def train(self, train_loader, model, optimizer, loss_fn):
+        model.train()
+        for (data, target) in tqdm(train_loader):
             data, target = data.to(self.device), target.to(self.device)
-            self.optimizer.zero_grad()
-            output = self.model(data)
-            loss = self.loss_fn(output, target)
+            optimizer.zero_grad()
+            output = model(data)
+            loss = loss_fn(output, target)
             loss.backward()
-            self.optimizer.step()
+            optimizer.step()
 
-    def test(self, epoch, test_loader, test_idx, val=True):
-        self.model.eval()
-        test_loss = 0
+    def test(self, epoch, test_loader, model, loss_fn, test_idx):
+        model.eval()
+        loss = 0
         correct = 0
-        output = None
         target = None
+        pred = None
         with torch.no_grad():
-            for data, target in tqdm(test_loader):
+            for data, target in test_loader:
                 data, target = data.to(self.device), target.to(self.device)
-                output = self.model(data)
-                test_loss += self.loss_fn(output, target, reduction='sum').item()  
+                output = model(data)
+                loss += loss_fn(output, target, reduction='sum').item()  
                 pred = torch.where(output >= 0.5, 1, 0) 
                 correct += pred.eq(target.view_as(pred)).sum().item()
 
-        test_loss /= len(test_idx)
+        loss /= len(test_idx)
         text = ""
-        text += f'\nTest set for epoch {epoch+1}: Average loss: {test_loss:.4f}, '
-        text += f'Accuracy: {correct}/{len(test_idx)} ({ 100. * correct / len(test_idx):.0f}%)\n'
+        text += f'\nTest set for epoch {epoch+1}: Average loss: {loss:.4f}, '
+        text += f'Accuracy: {correct}/{len(test_idx)} ({ correct / len(test_idx):.8f})\n'
         print(text)
 
-        if not val:
-            return torch.flatten(target), torch.flatten(pred)
+        if self.device.type == 'cuda':
+            target, pred = target.cpu(), pred.cpu()
+        metrics_dict = compute_metric_values(target.detach().numpy(), 
+                                             pred.detach().numpy())
+        return metrics_dict
 
         
 def main():
@@ -153,11 +195,7 @@ def main():
     'U2R': (1, array([0., 1., 0., 0., 0., 0., 0., 0.])),
     'Normal': (0, array([1., 0., 0., 0., 0., 0., 0., 0.]))}
     """
-    model = MLP()
-    runner = Runner(model=model, 
-                    optimizer=torch.optim.Adam(params=model.parameters()),
-                    loss_fn=F.binary_cross_entropy,
-                    ordinal_label=1)
+    runner = Runner(ordinal_label=1)
     runner.run()
 
 if __name__ == '__main__':
