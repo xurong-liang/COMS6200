394b095eded0fa35c5692b623cf18ca05a83cf2c
Jason Xurong Liang <xurong.liang@uqconnect.edu.au>
2022-10-18 16:17:52 +1000

    conducted major revision in knn.py

--- knn.py
+++ knn.py
@@ -8,35 +8,10 @@ from evaluate import compute_metric_values, generate_class_performance_text, sav
 import numpy as np
 import copy
 from collections import Counter
-from multiprocessing import Process
 from datetime import timedelta
 from argparse import ArgumentParser
-import sys
 import imblearn
-import os
-import re
-'''
-def check_has_undersampling(classifier_name: str, imbalanced_class: str,
-                            dataset: str,
-                            res_dir: str = "./res/address_imbalanced_res") -> bool:
-    """
-    Check whether we have called undersampling for current (classifier, imbalanced calss)
-    combination.
-    Since for over-sampling we have multiple sampling strategies so solve_imbalance_problem()
-    function will be called multiple times, but we do not have sampling strategies
-    for under-sampling, meaning we only need to call once
 
-    :param res_dir:
-    :return:
-    """
-    if os.path.exists(res_dir):
-        expr = f'{classifier_name}_{imbalanced_class}_(\w+)_AllKNN_(\w+)_'
-        _, _, _files = next(os.walk(res_dir), (None, None, []))
-        for _file in _files:
-            if _file.endswith(f"{dataset}.txt") and re.match(expr, _file):
-                return True
-    return False
-'''
 
 def solve_imbalance_problem(dataset: str, base_classifier: str, imbalanced_class: str,
                             sampling_strategy: float = .2):
@@ -49,9 +24,9 @@ def solve_imbalance_problem(dataset: str, base_classifier: str, imbalanced_class
     :param sampling_strategy: the percentage of minor class instances wrt to number
         of instances in the majority class
     """
-    over_samplings = ["SMOTE", "SMOTETomek", "SMOTEENN", ] 
+    over_samplings = ["SMOTE", "SMOTETomek", "SMOTEENN", ]
     under_samplings = ["AllKNN", "CondensedNearestNeighbour", "TomekLinks"]
-    class_imbalanced_methods = over_samplings 
+    class_imbalanced_methods = over_samplings
 
     class_imbalanced_methods_mapping = {
         # over-sampling strategies
@@ -88,9 +63,9 @@ def solve_imbalance_problem(dataset: str, base_classifier: str, imbalanced_class
             all_datasets_y["origin"] = batch_train_labels
         else:
             all_datasets_x["origin"] = np.concatenate((all_datasets_x["origin"],
-                                                      np.array(train_features)), axis=0)
+                                                       np.array(train_features)), axis=0)
             all_datasets_y["origin"] = np.concatenate((all_datasets_y["origin"],
-                                                      batch_train_labels), axis=0)
+                                                       batch_train_labels), axis=0)
 
     original_train_label_distributions = None
     for method in class_imbalanced_methods:
@@ -122,9 +97,9 @@ def solve_imbalance_problem(dataset: str, base_classifier: str, imbalanced_class
                 all_datasets_y[method] = y
             else:
                 all_datasets_x[method] = np.concatenate((all_datasets_x[method],
-                                                        np.array(X)), axis=0)
+                                                         np.array(X)), axis=0)
                 all_datasets_y[method] = np.concatenate((all_datasets_y[method],
-                                                        y), axis=0)
+                                                         y), axis=0)
 
             class_metrics[method]["new_train_label_distributions"].append(dict(Counter(y)))
             imbalanced_end = timer()
@@ -164,69 +139,71 @@ def solve_imbalance_problem(dataset: str, base_classifier: str, imbalanced_class
         # not purely under-samplings, add sampling strategy as hyper setting
         hyper_text += f"_sampling_strategy_{sampling_strategy}"
     print(performance_text, imbalanced_class)
-    f = open("knn_imbalanced.txt", "a")
-    f.write('sampling strategy: '+ sampling_strategy+ 'i_class: '+imbalanced_class+ "performance: "+ performance_text)
-    f.close()
-    '''
+    # f = open("knn_imbalanced.txt", "a")
+    # f.write(
+    #     'sampling strategy: ' + sampling_strategy + 'i_class: ' + imbalanced_class + "performance: " + performance_text)
+    # f.close()
     save_result_text(classifier=base_classifier + f"_{imbalanced_class}",
                      hyper=hyper_text, data_method=dataset,
                      class_performance_text=performance_text,
-                     res_dir= imbalanced_res_path)
-    folder_name = base_classifier + f"_{imbalanced_class}_" + hyper_text + "_" + dataset
+                     res_dir="./res/dir/address_imbalanced_res/")
+    # folder_name = base_classifier + f"_{imbalanced_class}_" + hyper_text + "_" + dataset
     # compute pca
-    plot_2_pc_results(dataset_x=all_datasets_x, dataset_y=all_datasets_y,
-                      res_dir=plot_path)
-    '''
+    # plot_2_pc_results(dataset_x=all_datasets_x, dataset_y=all_datasets_y,
+    #                   res_dir=plot_path)
 
-# def get_arguments() -> dict:
-#     """
-#     Initialize and get program input arguments
-#     :return: set of argument values in a ditionary
-#     """
-#     parser = ArgumentParser()
-#     parser.add_argument("--task", type=str,
-#                         help="What is the task to be conducted."
-#                              " Options: ['full', 'imbalance_problem']"
-#                         )
-#     parser.add_argument("--classifier", type=str, nargs="+",
-#                         help="What classifier to be used. "
-#                              "Options: ['adaboost', 'random_forest']"
-#                         )
-#     parser.add_argument("--dataset", type=str, nargs="+",
-#                         help="The type of dataset to be evaluated. "
-#                              "default: ['unnormalized', 'zscore', 'minmax']"
-#                         )
-#     parser.add_argument("--imbalanced_class", type=str,
-#                         help="The name of the imbalanced class to be evaluated.")
-#     parser.add_argument("--sampling_strategy", type=float, nargs="+",
-#                         help="The list of sampling strategies for SMOTE to compute.")
 
-#     task_range = ['full', 'imbalance_problem']
-#     classifier_range = ['both', 'adaboost', 'random_forest']
-#     dataset_range = ['unnormalized', 'zscore', 'minmax']
-#     parser.set_defaults(
-#         task="full",
-#         classifier="both",
-#         dataset=dataset_range,
-#         imbalanced_class="U2R",
-#         sampling_strategy=[.1 * k for k in range(1, 6)]
-#     )
-#     args = vars(parser.parse_args())
-#     # capitalize first letter of each word
-#     args["imbalanced_class"] = args["imbalanced_class"].title()
+def evaluate_a_data_frame(info: dict):
+    """
+    Process target function
 
-#     if args["task"] not in task_range:
-#         print(f"task must be in {task_range}", file=sys.stderr)
-#         exit(1)
-#     for classifier in args['classifier']:
-#         if classifier not in classifier_range:
-#             print(f"classifier must be in {classifier_range}", file=sys.stderr)
-#             exit(1)
-#     for dataset in args["dataset"]:
-#         if dataset not in dataset_range:
-#             print(f"{dataset} is not a valid dataset", file=sys.stderr)
-#             exit(1)
-#     return args
+    :param info: The dictionary that contains all information to evaluate a data frame
+    """
+    all_folder_indices = info["train_test_indices"]
+
+    data_frame = info["data_frame"]
+    data_method = info["data_method"]
+    one_hot_labels = info["one_hot_labels"]
+    one_pos_text_mapping = info['one_pos_text_mapping']
+    classifier_type = info["classifier_type"]
+    seed = info["seed"]
+    text_mapping = info["text_mapping"]
+    class_metrics = init_class_metrics(text_mapping)
+
+    for train_idxes, test_idxes in all_folder_indices:
+        train_features, train_labels = data_frame.iloc[train_idxes], one_hot_labels[train_idxes, :]
+        test_features, ground_truth = data_frame.iloc[test_idxes], one_hot_labels[test_idxes, :]
+
+        # one classifier per class
+        for one_pos, item in one_pos_text_mapping.items():
+            class_name = item[0]
+            batch_train_labels = train_labels[:, one_pos]
+            batch_ground_truth = ground_truth[:, one_pos]
+
+            assert not np.all(batch_ground_truth == 0) and not np.all(batch_train_labels == 0)
+            if class_name == "Normal":
+                # compute all combined scores
+                # normal class gets label 0, other classes all 1's
+                batch_train_labels = np.bitwise_not(batch_train_labels.astype(bool)).astype(float)
+                batch_ground_truth = np.bitwise_not(batch_ground_truth.astype(bool)).astype(float)
+            class_metrics = batch_runner(train_features, batch_train_labels,
+                                         test_features, batch_ground_truth, class_metrics,
+                                         classifier_type, seed, class_name=class_name)
+
+    for text_label in text_mapping.keys():
+        metrics = class_metrics[text_label]
+        for key, value in metrics.items():
+            metrics[key] = np.array(value)
+            if key == "training time":
+                metrics[key] = metrics[key].sum()
+            else:
+                metrics[key] = metrics[key].mean()
+
+    performance_text = generate_class_performance_text(res_dict=class_metrics)
+    save_result_text(classifier=classifier_type, hyper="default", data_method=data_method,
+                     class_performance_text=performance_text)
+    print(f'{classifier_type} results for {data_method} dataset completed.')
+
 
 def init_class_metrics(text_mapping: dict, imbalanced_problem: bool = False) -> dict:
     """
@@ -253,7 +230,6 @@ def init_class_metrics(text_mapping: dict, imbalanced_problem: bool = False) -> 
     return class_metrics
 
 
-
 def batch_runner(train_features, batch_train_labels, test_features,
                  batch_ground_truth, class_metrics, classifier_type,
                  seed, class_name) -> dict:
@@ -263,7 +239,7 @@ def batch_runner(train_features, batch_train_labels, test_features,
     :return: the updated class_metrics
     """
     if classifier_type == "knn":
-        classifier= KNeighborsClassifier()
+        classifier = KNeighborsClassifier()
     '''
     if classifier_type == "svm-linear":
         classifier = svm.LinearSVC(random_state=seed)
@@ -322,15 +298,18 @@ def test_a_batch(features, ground_truth, model):
 if __name__ == "__main__":
     argparse = ArgumentParser()
     argparse.add_argument("--run_full_program", type=lambda x: x.lower() == "true", required=True,
-    help="if true, then runs the whole program; otherwise, tries to address the imbalanced problem of U2R class")
+                          help="if true, then runs the whole program; otherwise, tries to address the imbalanced problem of U2R class")
+    argparse.add_argument("--sampling_strategy", type=float, nargs="+",
+                          help="The list of sampling strategies for SMOTE to compute.")
+    argparse.set_defaults(sampling_strategy=[round(.1 * _, 1) for _ in range(6)])
 
+    args = argparse.parse_args()
 
     # True if run full program;¨False otherwise (i.e., imbalanced dataset problem)
-    run_full_program = argparse.parse_args().run_full_program
-    seed= 2022
+    run_full_program = args.run_full_program
+    seed = 2022
     total_start = timer()
-    #processes = []
-    if run_full_program: 
+    if run_full_program:
         for method in ["minmax", "unnormalized", "zscore"]:
             for c_type in ['knn']:
                 # params = df, one_hot_labels, text_mapping, ordinal_mapping, one_pos_text_mapping
@@ -344,21 +323,15 @@ if __name__ == "__main__":
                     'text_mapping': params[2],
                     'one_pos_text_mapping': params[4],
                     "classifier_type": c_type,
-                    "seed": 2022,
+                    "seed": seed,
                     "data_method": method
                 }
                 evaluate_a_data_frame(inputs)
     else:
-        for _s in [round(.1 * _, 1) for _ in range(5, 6)]:
-                print(f"current sampling strategy: {_s}")
-                solve_imbalance_problem(dataset= 'minmax', base_classifier='knn', imbalanced_class= 'U2R',
-                            sampling_strategy=_s)
-           #p = Process(target=evaluate_a_data_frame, args=(inputs,))
-            #print(f"Now start {c_type} on {method} dataset")
-            #p.start()
-            #processes.append(p)
-
-    #for p in processes:
-     #   p.join()
+        s_strategy = args.sampling_strategy
+        for _s in s_strategy:
+            print(f"current sampling strategy: {_s}")
+            solve_imbalance_problem(dataset='minmax', base_classifier='knn', imbalanced_class='U2R',
+                                    sampling_strategy=_s)
     total_end = timer()
     print(f'All processes finished, total elapsed time {timedelta(seconds=total_end - total_start)}.')
